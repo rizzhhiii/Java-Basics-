4d1af50c5ff264d8a9a455b3046277f8b70e08f2
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-05-11 10:53:41 +0530

    Leant sum of varaiables.

---
+++ Sumofvariables.java
@@ -0,0 +1,9 @@
+public class Sumofvariables{
+    public static void main(String args[]){
+        // code to calculate sum
+        int a = 10;
+        int b = 89;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
