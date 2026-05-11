b2c4060be9e6e5aa8a6199f48d7ba6dda86df9df
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-05-11 10:50:58 +0530

    Added Variables.java practice program

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables{
+    public static void main(String args[]){
+    int a = 10;
+    int b = 77;
+    System.out.println(a);
+        System.out.println(b);
+        System.out.println(a=b);
+
+
+
+    
+    }
+}
