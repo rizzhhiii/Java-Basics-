261973e51d393c9f82c29e2b2d494ae06fcee55f
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-05-12 22:11:46 +0530

    Added total bill calculator using float inputs

---
+++ Totalbill.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Totalbill{
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+
+     float pencil = sc.nextFloat();
+     float pen = sc.nextFloat();
+     float eraser = sc.nextFloat();
+     float sum = pencil + pen + eraser;
+
+        System.out.println(sum);
+    }
+}
