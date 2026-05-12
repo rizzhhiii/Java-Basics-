f71ab1581e291176bf0c8cdc9d536a21d01ea1f9
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-05-12 22:10:58 +0530

    Learned explicit typecasting in Java

---
+++ Typecasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Typecasting{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+     float a = 25.14f;
+     int b = (int) a;
+System.out.println(b);
+    }
+} 
