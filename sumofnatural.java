2c5d71a348dacc78203d2e23988577ab1b5935a4
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-07-24 15:01:09 +0530

    sum of n natural numbers

---
+++ sumofnatural.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class sumofnatural{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        while(i <= n){
+        sum += i;
+        i++;
+        }
+
+        System.out.println("The sum is : " + sum);
+    
+    
+        
+
+    
+    }
+}
