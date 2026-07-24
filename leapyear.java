91bd4323c1cac29300fd797ccbfac926fd4be182
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-07-24 14:59:49 +0530

    To find if the year is leap or non - leap year

---
+++ leapyear.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class leapyear{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year");
+
+        int year = sc.nextInt();
+
+        if( year % 400 == 0){
+            System.out.println(" The year is  a leap year");
+        }
+
+            else if( year % 100 == 0){
+                System.out.println(" the year is not a leap year");
+
+            }
+
+        else if( year % 4 == 0){
+            System.out.println(" The year is a leap year");
+        }
+
+        else{
+            System.out.println(" The year is not a leap year");
+        }
+        
+    }
+
+}
