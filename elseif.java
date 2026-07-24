c4d86e5d959686a8df9302682d3b1bfd3de6f20a
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-07-24 14:54:57 +0530

    else if

---
+++ elseif.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ 
+ public class elseif{
+    public static void main(String args[]){
+
+        int age = 9;
+
+        if(age > 18){
+            System.out.println("You are an adult");
+}else if (age >= 13 && age < 18){
+    System.out.println("You are a teenager");
+
+} else{
+
+    System.out.println("You are a child");
+}
+    }
+
+
+ }
