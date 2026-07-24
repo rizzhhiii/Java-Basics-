90e9eedb880819abd135737799dc1268e16644ab
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-07-24 14:53:24 +0530

    Add files via upload
    Find the largest of two numbers

---
+++ largestoftwonumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class largestoftwonumbers{ 
+public static void main(String args[]){
+
+int a = 23;
+int b = 99;
+
+if(a > b){
+    System.out.println("b");
+}
+
+    else{
+        System.out.println("a");
+    }
+}
+}
