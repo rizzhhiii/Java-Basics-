8a31609e4704b6be0eeb0fc6a91f838184246e1d
Rishikesh Chattersing Rajput <rizzhhiii@gmail.com>
2026-07-24 15:00:38 +0530

    learnt about while loop

---
+++ whileloop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public class whileloop{
+public static void main(String args[]){
+
+    int counter = 0;
+    while(counter < 100){
+        System.out.println("HELLO WORLD");
+        counter++;
+    }
+     System.out.println("HELLO WORLD");
+}
+}
